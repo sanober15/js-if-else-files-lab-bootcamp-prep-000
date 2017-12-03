3f3c07929846d46b59efd1d6580c3e0bb5bef86a
sanober15 <sanober@sanober.me>
2017-12-03 00:18:32 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,12 +1,12 @@
 var index = require("./index.js")
 
-if (index.name === "susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: susan, Received: "+index.name)
 }
 
-if (index.height === 70) {
+if (index.height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
